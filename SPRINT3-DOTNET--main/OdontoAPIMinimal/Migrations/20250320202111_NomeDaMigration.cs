6ceb57fce91253d097a4e8b5b960b80ecef5e07e
glenda <glendadelfy20@gmail.com>
2025-03-20 22:07:08 -0300

    projeto para devops

---
+++ SPRINT3-DOTNET--main/OdontoAPIMinimal/Migrations/20250320202111_NomeDaMigration.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OdontoAPIMinimal.Migrations
+{
+    /// <inheritdoc />
+    public partial class NomeDaMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "status_active_user",
+                table: "table_usuario_usuario_odonto",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "NUMBER(1)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "role_user",
+                table: "table_usuario_usuario_odonto",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(2000)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "password_user",
+                table: "table_usuario_usuario_odonto",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(2000)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name_user",
+                table: "table_usuario_usuario_odonto",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(2000)");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "is_complete",
+                table: "table_usuario_usuario_odonto",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "NUMBER(1)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "email_user",
+                table: "table_usuario_usuario_odonto",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(2000)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "avatar_user",
+                table: "table_usuario_usuario_odonto",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(2000)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "table_usuario_usuario_odonto",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "NUMBER(10)")
+                .Annotation("SqlServer:Identity", "1, 1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "status_active_user",
+                table: "table_usuario_usuario_odonto",
+                type: "NUMBER(1)",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "role_user",
+                table: "table_usuario_usuario_odonto",
+                type: "NVARCHAR2(2000)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "password_user",
+                table: "table_usuario_usuario_odonto",
+                type: "NVARCHAR2(2000)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name_user",
+                table: "table_usuario_usuario_odonto",
+                type: "NVARCHAR2(2000)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "is_complete",
+                table: "table_usuario_usuario_odonto",
+                type: "NUMBER(1)",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "email_user",
+                table: "table_usuario_usuario_odonto",
+                type: "NVARCHAR2(2000)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "avatar_user",
+                table: "table_usuario_usuario_odonto",
+                type: "NVARCHAR2(2000)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "table_usuario_usuario_odonto",
+                type: "NUMBER(10)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+        }
+    }
+}
